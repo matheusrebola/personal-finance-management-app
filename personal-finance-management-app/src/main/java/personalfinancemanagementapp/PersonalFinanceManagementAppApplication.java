be46c787408eccd4ea31bc480d42d6dcba282d2f
matheusrebola <m.cavalcantir@outlook.com>
2024-07-17 23:21:48 -0300

    feat: criação do projeto spring

---
+++ personal-finance-management-app/src/main/java/personalfinancemanagementapp/PersonalFinanceManagementAppApplication.java
@@ -0,0 +1,13 @@
+package personalfinancemanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceManagementAppApplication.class, args);
+	}
+
+}
---
+++ personal-finance-management-app/src/test/java/personalfinancemanagementapp/PersonalFinanceManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package personalfinancemanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
